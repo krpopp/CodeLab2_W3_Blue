9a6145646a74944553dde61959704f146e037ea6
Zhiyue Huang <zhiyuehuang@yeah.net>
2022-09-18 12:48:33 -0400

    Add vertical match

--- Assets/Students/zh2052/Scripts/ZHMatchManager.cs
+++ Assets/Students/zh2052/Scripts/ZHMatchManager.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class ZHMatchManager : MatchManagerScript
 {
+	//protected GameManagerScript gameManager;
+
 	public override bool GridHasMatch()
 	{
 		bool hasMatch = base.GridHasMatch();
@@ -20,7 +22,7 @@ public class ZHMatchManager : MatchManagerScript
 		return hasMatch;
 	}
 
-	bool GridHasVerticalMatch(int x, int y)
+	public bool GridHasVerticalMatch(int x, int y)
     {
 		// check vertical matches
 		GameObject token1 = gameManager.gridArray[x, y + 0];
@@ -40,4 +42,74 @@ public class ZHMatchManager : MatchManagerScript
 			return false;
 		}
     }
+
+	public int GetVerticalMatchLength(int x, int y)
+    {
+		int matchLength = 1;
+
+		GameObject first = gameManager.gridArray[x, y];
+
+		if (first != null)
+		{
+			SpriteRenderer sr1 = first.GetComponent<SpriteRenderer>();
+
+			for (int i = y + 1; i < gameManager.gridHeight; i++)
+			{
+				GameObject other = gameManager.gridArray[x, i];
+
+				if (other != null)
+				{
+					SpriteRenderer sr2 = other.GetComponent<SpriteRenderer>();
+
+					if (sr1.sprite == sr2.sprite)
+					{
+						matchLength++;
+					}
+					else
+					{
+						break;
+					}
+				}
+				else
+				{
+					break;
+				}
+			}
+		}
+
+		return matchLength;
+	}
+
+    public override int RemoveMatches()
+    {
+		int numRemoved = base.RemoveMatches();
+
+		for (int x = 0; x < gameManager.gridWidth; x++)
+		{
+			for (int y = 0; y < gameManager.gridHeight; y++)
+			{
+				if (y < gameManager.gridHeight - 2)
+				{
+					// get vertical match length
+					int verticalMatchLength = GetVerticalMatchLength(x, y);
+
+					if (verticalMatchLength > 2)
+					{
+
+						for (int i = y; i < y + verticalMatchLength; i++)
+						{
+							GameObject token = gameManager.gridArray[x, i];
+							Destroy(token);
+
+							gameManager.gridArray[x, i] = null;
+							numRemoved++;
+						}
+					}
+				}
+			}
+		}
+
+		return numRemoved;
+    }
+
 }
