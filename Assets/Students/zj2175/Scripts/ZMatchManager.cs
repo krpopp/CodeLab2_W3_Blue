77a18b4423ac88603dc47971fc118006731eba96
Zachary Jones <>
2022-09-20 18:28:27 -0400

    added  ZMatchManager

--- Assets/Students/zj2175/Scripts/ZMatchManager.cs
+++ Assets/Students/zj2175/Scripts/ZMatchManager.cs
@@ -2,17 +2,32 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ZMatchManager : MonoBehaviour
+public class ZMatchManager : MatchManagerScript
 {
-    // Start is called before the first frame update
-    void Start()
+    public override bool GridHasMatch()
     {
-        
+        bool hasMatch = base.GridHasMatch();
+        for(int x = 0; x < gameManager.gridWidth; x++)
+        {
+            for(int y = 0; y < gameManager.gridHeight - 2; y++)
+            {
+                hasMatch = hasMatch || GridHasVerticalMatch(x, y);
+            }
+        }
+
+        return hasMatch;
     }
 
-    // Update is called once per frame
-    void Update()
+    public bool GridHasVerticalMatch(int x, int y)
     {
+        //find if grid has vertical match
+        return true;
+    }
+
+    public override int RemoveMatches()
+    {
+        int numRemoved = base.RemoveMatches();
         
+        return numRemoved;
     }
 }
